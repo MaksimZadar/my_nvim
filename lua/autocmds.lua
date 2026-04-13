2bc9eca5397e3cc1d21189842af4f4d19a36383b
Max Zadarnovskiy <mzadar@pm.me>
2026-04-13 17:08:22 +0930

    auto open nvtree when starting

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,10 @@
 require "nvchad.autocmds"
+
+-- Open NvimTree on startup when launching without a file
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 and vim.api.nvim_buf_get_name(0) == "" then
+      require("nvim-tree.api").tree.open()
+    end
+  end,
+})
